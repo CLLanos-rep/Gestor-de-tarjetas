79ec293efe1dd41da55dbc19f79b579d9e40e648
Cristian Ll <cllanos.rep@gmail.com>
2025-04-27 02:10:31 -0400

    Modificado GestorDeTarjetas añadiendo metodos para obtencion de lista de transacciones y saldo disponible

--- src/com/cll/gestordecompras/modelos/GestorDeTarjetas.java
+++ src/com/cll/gestordecompras/modelos/GestorDeTarjetas.java
@@ -1,6 +1,8 @@
 package com.cll.gestordecompras.modelos;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GestorDeTarjetas {
@@ -20,5 +22,17 @@ public class GestorDeTarjetas {
         var tarjeta = tarjetas.get(id);
         if (tarjeta == null) return false;
         return tarjeta.procesarCompra(valor, descripcion);
+    }
+
+    // Obtener la lista de compras o transacciones, si no existe la tarjeta entrega lista vacia.
+    public List<Compra> getTransacciones(String id) {
+        var tarjeta = tarjetas.get(id);
+        return tarjeta == null ? Collections.emptyList() : tarjeta.getTransacciones();
+    }
+
+    // Obtener saldo disponible en la tarjeta por id
+    public double getSaldoDisponible (String id) {
+        var tarjeta = tarjetas.get(id);
+        return tarjeta == null ? 0 : tarjeta.getCupo() - tarjeta.getSaldo();
     }
 }
